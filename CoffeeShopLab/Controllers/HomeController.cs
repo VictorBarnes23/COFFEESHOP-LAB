68b236820ec893ff3cebc651216e868c10edd2fe
Victor <vicpbarnes17@gmail.com>
2024-02-21 17:25:46 -0500

    First

---
+++ CoffeeShopLab/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using CoffeeShopLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(Account a)
+        {
+            return View(a);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopLab/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeShopLab.Models
+{
+    public class Account
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
